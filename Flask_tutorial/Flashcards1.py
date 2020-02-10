e4664ed914e15853155688fd7f84e5f9f5e4b69a
Kiranmayee Dobbali <kiranmayee.dobbali@gmail.com>
2020-02-10 10:32:59 -0700

    import resolved

---
+++ Flask_tutorial/Flashcards1.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, abort
+from model import db
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route("/")
+def welcome():
+    return render_template("welcome.html",
+                           message = "Here is a message from view")
+
+
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card = card, index = index)
+    except IndexError:
+         abort(404)
