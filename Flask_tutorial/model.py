b1ccdaae5c2d55f4d128c719995cdb27392d34c4
Kiranmayee Dobbali <kiranmayee.dobbali@gmail.com>
2020-02-21 17:51:27 -0700

    before nginix nd gunicorn

--- Flask_tutorial/model.py
+++ Flask_tutorial/model.py
@@ -4,8 +4,15 @@ import json
 
 
 def load_db():
-    with open("flashcards_db.json") as f:
+    with open("flashcards_db.json", encoding='utf-8') as f:
         return json.load(f)
 
 
+
+def save_db():
+    with open("flashcards_db.json", 'w') as f:
+        return json.dump(db, f)
+
 db = load_db()
+
+
